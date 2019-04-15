26f2c7ccf85346dd510f20bf2ceb7a766de673c8
solcream <47736779+solcream@users.noreply.github.com>
2019-04-15 20:22:28 +0200

    commit

---
+++ 03_03/finished/react-is-fun/src/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+let skiData = {
+	total: 50,
+	powder: 20,
+	backcountry: 10,
+	goal: 100
+}
+
+class SkiDayCounter extends Component {
+	render() {
+		const {total, powder, backcountry, goal} = this.props
+		return (
+			<section>
+				<div>
+					<p>Total Days: {total}</p>
+				</div>
+				<div>
+					<p>Powder Days: {powder}</p>
+				</div>
+				<div>
+					<p>Backcountry Days: {backcountry}</p>
+				</div>
+				<div>
+					<p>Goal: {goal}</p>
+				</div>
+			</section>
+		)
+	}
+}
+
+render(
+	<SkiDayCounter 
+		total={skiData.total}
+		powder={skiData.powder}
+		backcountry={skiData.backcountry}
+		goal={skiData.goal}/>, 
+	document.getElementById('root')
+)
