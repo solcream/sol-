915f66d0b43a527b5455b87db1315dc099b03722
solcream <47736779+solcream@users.noreply.github.com>
2019-04-15 21:01:52 +0200

    commit

---
+++ 05_08/finished/react-is-fun/build/precache-manifest.4be35f1d4db7b6fab9ae8c9f9037f2ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2214ea9ff89ec38e80e4",
+    "url": "/static/js/main.2214ea9f.chunk.js"
+  },
+  {
+    "revision": "11640f626343bf39081e",
+    "url": "/static/js/1.11640f62.chunk.js"
+  },
+  {
+    "revision": "4f786a8d8ec795d0448b9240d65d1a79",
+    "url": "/index.html"
+  }
+];
